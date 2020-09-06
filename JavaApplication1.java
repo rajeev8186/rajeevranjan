1d9aed5e81f1aed8a3a6e5c982726ffb7386945e
rajeev8186 <59604081+rajeev8186@users.noreply.github.com>
2020-09-06 23:22:03 +0530

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,19 @@
+
+package javaapplication1;
+
+
+public class JavaApplication1 
+{
+
+    
+    public static void main(String[] args) 
+    {
+        int a=10;
+        int b=20;
+        int sum=0;
+        sum=a+b;
+        System.out.println("sum of two number is: "+sum);
+        // TODO code application logic here
+    }
+    
+}
